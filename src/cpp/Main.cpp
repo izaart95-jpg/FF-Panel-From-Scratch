7b7ae84545cf87c2d310a64da4dc6518edaea0b6
Lelouch <izaart95@gmail.com>
2026-05-31 16:11:32 +0530

    Update Main.cpp

--- src/cpp/Main.cpp
+++ src/cpp/Main.cpp
@@ -11,8 +11,8 @@
 #include <iomanip>
 #include <sstream>
 
-static std::string originalBytes = "9A 99 99 3E FF FF FF FF 08 00 00 00 00 00 60 40 CD CC 8C 3F 8F C2 F5 3C CD CC CC 3D 06 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 33 33 13 40 00 00 B0 3F 00 00 80 3F 01";
-static std::string patchedBytes  = "9A 99 99 3E FF FF FF FF 08 00 00 00 00 00 60 40 CD CC 8C 3F 8F C2 F5 3C CD CC CC 3D 06 00 00 00 00 00 19 3F 00 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 33 33 13 40 00 00 B0 3F 00 00 80 3F 01";
+static std::string originalBytes = ""; // AoB to scan
+static std::string patchedBytes  = ""; // AoB to replace
 
 class Internal {
 public:
